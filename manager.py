e8fd2e7e8f832555f24b21fecef4f1d08ba9adaa
xwp <xwp_fullstack@163.com>
2018-10-15 08:38:59 +0800

    redis数据库配置

--- manager.py
+++ manager.py
@@ -17,24 +17,39 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
+
+
 app = Flask(__name__)
+
+
 class Config(object):
     # 调试模式
-    DEBUG =True
+    DEBUG = True
 
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@localhost:3306/information16"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    # Redis配置
+    REIDS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 app.config.from_object(Config)
 
 # 创建SQLAlchemy对象，关联app
 db = SQLAlchemy(app)
 
-
+# 创建redis对象
+redis_store = redis.StrictRedis(host=Config.REIDS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 
 @app.route("/")
 def hello_world():
+
+    redis_store.set("name","laowang")
+    print(redis_store.get("name"))
+
     return "helloworld100"
 
 
