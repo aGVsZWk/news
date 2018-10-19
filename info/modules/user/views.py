2548ed0ba8efbe72bf88b61882781916cbc268df
xwp <xwp_fullstack@163.com>
2018-10-19 22:45:13 +0800

    个人中心显示右上角数据&首页跳转

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,5 +1,7 @@
+from flask import g,redirect,render_template
+
+from info.utils.commons import user_login_data
 from . import user_blue
-from flask import render_template
 
 # 功能: 获取用户个人中心页面
 # 请求路径: /user/info
@@ -7,9 +9,15 @@ from flask import render_template
 # 请求参数:无
 # 返回值: user.html页面,用户字典data数据
 @user_blue.route('/info')
+@user_login_data
 def user_info():
-    data = {
+    # 判断用户是否有登陆
+    if not g.user:
+        return redirect("/")
 
+    # 拼接数据，渲染页面
+    data = {
+        "user_info":g.user.to_dict()
 
     }
     return render_template("news/user.html",data=data)
