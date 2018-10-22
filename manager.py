1145803cf1ebf1f838a872cba59997293c4b3304
xwp <xwp_fullstack@163.com>
2018-10-22 11:52:38 +0800

    管理员用户创建

--- manager.py
+++ manager.py
@@ -21,7 +21,9 @@ from info import create_app,db,models
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
 
-app = create_app("develop")
+from info.models import User
+
+app = create_app("develop")\
 
 # 创建Manager对象，关联app
 manager = Manager(app)
@@ -30,6 +32,28 @@ manager = Manager(app)
 Migrate(app,db)
 
 manager.add_command("db",MigrateCommand)
+
+# 创建管理员方法
+@manager.option("-p","--password",dest="password")
+@manager.option("-u","--username",dest="username")
+def create_superuser(username,password):
+    # 1.创建管理员对象
+    admin = User()
+
+    # 2.设置属性
+    admin.nick_name = username
+    admin.password = password
+    admin.mobile = username
+    admin.is_admin = True
+
+    # 3.保存管理员到数据库
+    try:
+        db.session.add(admin)
+        # db.session.commit()
+    except Exception as e:
+        return "注册失败"
+
+    return "注册成功"
 
 
 if __name__ == '__main__':
