5399f8d3cbcffbd42e6bc21c24b1c49ae645e69e
xwp <xwp_fullstack@163.com>
2018-10-22 16:27:07 +0800

    总人数&月活&日活统计

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import time
 from flask import current_app
 from flask import g
 from flask import redirect
@@ -8,6 +10,68 @@ from flask import session
 from info.models import User
 from info.utils.commons import user_login_data
 from . import admin_blue
+
+
+# 用户统计
+# 请求路径：/admin/user_count
+# 请求方式：GET
+# 请求参数：无
+# 返回值：渲染页面 user_count.html，字典数据
+@admin_blue.route('/user_count')
+def user_count():
+    """
+    1.查询总人数，不包含管理员
+    2.查询月活人数
+    3.查询日活人数
+    4.时间段内的，活跃人数
+    5.携带数据，渲染页面
+    :return:
+    """
+    # 1.查询总人数，不包含管理员
+    try:
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger(e)
+
+    # 2.查询月活人数
+    cal = time.localtime()
+    try:
+        # 2.1 本月1号的0点的字符串表示
+        month_startTime_str = "%d-%d-01"%(cal.tm_year,cal.tm_mon)
+        month_startTime_data = datetime.strptime(month_startTime_str,"%Y-%m-%d")
+
+        # 2.2 此时的时间
+        month_endTime_data = datetime.now()
+
+        # 2.3 查询时间段内的人数
+        month_count = User.query.filter(User.last_login >= month_startTime_data,User.last_login <= month_endTime_data, User.is_admin ==False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+
+    # 3.查询日活人数
+    try:
+        # 2.1 本月1号的0点的字符串表示
+        day_startTime_str = "00-00-00"
+        day_startTime_data = datetime.strptime(day_startTime_str,"%H-%M-%S")
+
+        # 2.2 此时的时间
+        day_endTime_data = datetime.now()
+
+        # 2.3 查询时间段内的人数
+        day_count = User.query.filter(User.last_login >= day_startTime_data,User.last_login <= day_endTime_data, User.is_admin ==False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 4.时间段内的，活跃人数
+    # 5.携带数据，渲染页面
+    data = {
+        "total_count":total_count,
+        "month_count":month_count,
+        "day_count":day_count
+    }
+    return render_template("admin/user_count.html",data=data)
+
 # 显示管理员首页界面
 # 请求路径：/admin/index
 #　请求方式： GET
