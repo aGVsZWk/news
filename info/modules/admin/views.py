fc003fd7aa6b5d32599bdb406126dc7efb0865b4
xwp <xwp_fullstack@163.com>
2018-10-24 23:02:25 +0800

    用户列表统计

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import time
-from flask import current_app
+from flask import current_app, jsonify
 from flask import g
 from flask import redirect
 from flask import render_template
@@ -9,7 +9,61 @@ from flask import session
 
 from info.models import User
 from info.utils.commons import user_login_data
+from info.utils.response_code import RET
 from . import admin_blue
+
+
+# 功能描述：用户列表统计
+# 请求路径：/admin/user_list
+# 请求方式：GET
+# 请求参数：p
+# 返回值：渲染user_list.html页面，data字典数据
+@admin_blue.route("/user_list")
+def user_list():
+    """
+    1.获取参数
+    2.参数类型转换
+    3.分页查询
+    4.获取分页对象属性，总页数，当前页，当前页对象
+    5.将对象列表，转成字典列表
+    6.携带数据渲染页面
+    :return:
+    """
+    # 1.获取参数
+    page = request.args.get("p","1")
+
+    # 2.参数类型转换
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    # 3.分页查询
+    try:
+        paginate = User.query.filter(User.is_admin == False).order_by(User.create_time.desc()).paginate(page,10,False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="获取用户失败")
+
+    # 4.获取分页对象属性，总页数，当前页，当前页对象
+    totalPage = paginate.pages
+    currentPage = paginate.page
+    items = paginate.items
+
+    # 5.将对象列表，转成字典列表
+    user_list = []
+    for user in items:
+        user_list.append(user.to_admin_dict())
+
+    # 6.携带数据渲染页面
+    data = {
+        "totalPage":totalPage,
+        "currentPage":currentPage,
+        "user_list":user_list
+    }
+
+    return render_template("admin/user_list.html",data=data)
 
 
 # 用户统计
