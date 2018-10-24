7884f8c55e2eb96ac573af5557f480ef396fe8c3
xwp <xwp_fullstack@163.com>
2018-10-25 00:58:40 +0800

    新闻编辑列表

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -11,7 +11,63 @@ from info.models import User, News
 from info.utils.commons import user_login_data
 from info.utils.response_code import RET
 from . import admin_blue
+# 功能描述:新闻编辑列表
+# 请求路径：/admin/news_edit
+# 请求方式：GET
+# 请求参数：GET，P,keywords
+# 返回值：GET，渲染news_edit.html页面，data字典数据
+@admin_blue.route('/news_edit')
+def news_edit():
+    """
+    1.获取参数
+    2.参数类型转换
+    3.分页查询
+    4.获取分页对象属性，总页数，当前页，当前页对象
+    5.将对象列表，转成字典列表
+    6.携带数据渲染界面
+    :return:
+    """
 
+    # 1.获取参数
+    page = request.args.get("p","1")
+    keywords = request.args.get("keywords")
+
+    # 2.参数类型转换
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    # 3.分页查询
+    try:
+        # 判断，是否有搜索关键字
+        filters = []
+        if keywords:
+            filters.append(News.title.contains(keywords))
+
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,10,False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="获取新闻失败")
+
+    # 4.获取分页对象属性，总页数，当前页，当前页对象
+    totalPage = paginate.pages
+    currentPage = paginate.page
+    items = paginate.items
+
+    # 5.将对象列表，转成字典列表
+    news_list = []
+    for news in items:
+        news_list.append(news.to_review_dict())
+
+    # 6.携带数据渲染界面
+    data = {
+        "totalPage":totalPage,
+        "currentPage":currentPage,
+        "news_list":news_list
+    }
+    return render_template("admin/news_edit.html",data=data)
 
 # 功能描述：新闻审核详情
 # 请求路径：/admin/news_review_detail
